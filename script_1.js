413cff50027dc928823ee924bd76856c0e57a71b
marine.calderon <marine.calderon@gmail.com>
2019-11-14 09:32:03 +0100

    ajout des fichiers

---
+++ script_1.js
@@ -0,0 +1,2 @@
+ name = prompt("Quel est ton prénom") // La boîte de dialogue prompt() permet d’afficher un message, un champ à remplir (imput de type texte) et un bouton « OK ». Cette fonction retourne la valeur qui a été entré dans le champ par l’utilisateur.
+console.log(`Bonjour ${name}!`);
---
+++ script_2.js
@@ -0,0 +1,9 @@
+number = prompt("De quel nombre veux-tu calculer la factorielle ?");
+
+
+function factorial(n) {
+     return (n != 1) ? n * factorial(n - 1) : 1; // Par définition, une factorielle est n! peut être écrit comme n * (n-1) !.
+                                                //  donc le résultat de la factorielle (n) peut être calculé comme étant n multiplié par le résultat de la factorielle (n-1). Et l'appel de n-1 peut descendre récursivement plus bas et plus bas jusqu'à 1.
+}
+
+console.log(factorial(number)); //on retourne donc la factorielle du nombre entré
